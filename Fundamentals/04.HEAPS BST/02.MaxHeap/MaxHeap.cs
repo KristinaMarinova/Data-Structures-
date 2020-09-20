7cc773f9750b03a4a13155dd2e5282e9061c7efd
Kristina <kriska2000@abv.bg>
2020-09-20 21:04:46 +0300

    second task is completed

--- Fundamentals/04.HEAPS BST/02.MaxHeap/MaxHeap.cs
+++ Fundamentals/04.HEAPS BST/02.MaxHeap/MaxHeap.cs
@@ -1,20 +1,67 @@
 ﻿namespace _02.MaxHeap
 {
     using System;
+    using System.Collections.Generic;
 
     public class MaxHeap<T> : IAbstractHeap<T>
         where T : IComparable<T>
     {
-        public int Size { get { throw new NotImplementedException(); } }
+        private List<T> _elements;
+
+        public MaxHeap()
+        {
+            this._elements = new List<T>();
+        }
+        public int Size => this._elements.Count;
+        public T Peek()
+        {
+            this.EnsureNotEmpty();
+            return this._elements[0];
+        }
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this._elements.Add(element);
+
+            this.HeapifyUp();
         }
 
-        public T Peek()
+        private void EnsureNotEmpty()
         {
-            throw new NotImplementedException();
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException("Max heap is empty");
+            }
+        }
+        private bool IndexIsValid(int index)
+        {
+            return index > 0;
+        }
+        private bool IsGreater(int childIndex, int parentIndex)
+        {
+            return this._elements[childIndex].CompareTo(this._elements[parentIndex]) > 0;
+        }
+        private int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+        private void Swap(int currentIndex, int parentIndex)
+        {
+            var temp = this._elements[currentIndex];
+            this._elements[currentIndex] = this._elements[parentIndex];
+            this._elements[parentIndex] = temp;
+        }
+        private void HeapifyUp()
+        {
+            int currentIndex = this.Size - 1;
+            int parentIndex = this.GetParentIndex(currentIndex);
+            while (this.IndexIsValid(currentIndex) && this.IsGreater(currentIndex, parentIndex))
+            {
+                this.Swap(currentIndex, parentIndex);
+                currentIndex = parentIndex;
+                parentIndex = this.GetParentIndex(currentIndex);
+
+            }
         }
     }
 }
