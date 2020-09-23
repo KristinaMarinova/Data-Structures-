f2d7f8ab748c90eef6b940e8a43c5b753c82a8e3
Kristina <kriska2000@abv.bg>
2020-09-23 14:56:48 +0300

    completed task

--- Fundamentals/04.HEAPS BST/Exercise/02.LowestCommonAncestor/BinaryTree.cs
+++ Fundamentals/04.HEAPS BST/Exercise/02.LowestCommonAncestor/BinaryTree.cs
@@ -1,6 +1,8 @@
 ﻿namespace _02.LowestCommonAncestor
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class BinaryTree<T> : IAbstractBinaryTree<T>
         where T : IComparable<T>
@@ -10,7 +12,9 @@
             BinaryTree<T> leftChild,
             BinaryTree<T> rightChild)
         {
-            throw new NotImplementedException();
+            this.Value = value;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
         }
 
         public T Value { get; set; }
@@ -23,7 +27,48 @@
 
         public T FindLowestCommonAncestor(T first, T second)
         {
-            throw new NotImplementedException();
+            List<T> firstPath = findPath(first);
+            List<T> secondPath = findPath(second);
+
+            int smallerSize = firstPath.Count() < secondPath.Count() ? firstPath.Count() : secondPath.Count();
+
+            var i = 0;
+            for (; i < smallerSize; i++)
+            {
+                if (!firstPath[i].Equals(secondPath[i]))
+                {
+                    break;
+                }
+            }
+
+            return firstPath[i - 1];
+        }
+
+        private List<T> findPath(T element)
+        {
+            List<T> result = new List<T>();
+            findNodePath(this, element, result);
+
+            return result;
+        }
+
+        private bool findNodePath(BinaryTree<T> binaryTree, T element, List<T> currentPath)
+        {
+            if (binaryTree == null) { return false; }
+            if (binaryTree.Value.Equals(element)) { return true; }
+
+            currentPath.Add(binaryTree.Value);
+
+            bool leftResult = findNodePath(binaryTree.LeftChild, element, currentPath);
+            if (leftResult) { return true; }
+
+            bool rightResult = findNodePath(binaryTree.RightChild, element, currentPath);
+            if (rightResult) { return true; }
+
+
+            currentPath.Remove(binaryTree.Value); // value of
+
+            return false;
         }
     }
 }
