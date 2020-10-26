875289326f9191b331f88b40caae166c4f05884d
Kristina <kriska2000@abv.bg>
2020-10-26 23:34:20 +0200

    add and remove methods

--- Advanced/01. B-Trees-2-3-Trees-and-AVL-Trees/Exercise/02. B-Trees-2-3-Trees-and-AVL-Trees-Exercise-Skeleton/01.Hierarchy/Hierarchy.cs
+++ Advanced/01. B-Trees-2-3-Trees-and-AVL-Trees/Exercise/02. B-Trees-2-3-Trees-and-AVL-Trees-Exercise-Skeleton/01.Hierarchy/Hierarchy.cs
@@ -6,19 +6,39 @@
 
     public class Hierarchy<T> : IHierarchy<T>
     {
+        private Node<T> root;
+        private Dictionary<T, Node<T>> elements = new Dictionary<T, Node<T>>();
+        public Hierarchy(T root)
+        {
+            this.root = CreateNode(root);
+        }
 
-        public int Count { get; }
+        public int Count { get => elements.Count; }
 
         public void Add(T element, T child)
         {
-            throw new NotImplementedException();
+            ContainsItemOrThrowExeption(element);
+
+            if (elements.ContainsKey(child))
+            {
+                throw new ArgumentException();
+            }
+            var node = CreateNode(child);
+            node.Parent = elements[element];
+            elements[element].Children.Add(node);
         }
 
         public void Remove(T element)
         {
-            throw new NotImplementedException();
-        }
+            if (this.root.Value.Equals(element))
+            {
+                throw new InvalidOperationException();
+            }
 
+            ContainsItemOrThrowExeption(element);
+
+            DestroyElement(element);
+        }
         public IEnumerable<T> GetChildren(T element)
         {
             throw new NotImplementedException();
@@ -31,7 +51,7 @@
 
         public bool Contains(T element)
         {
-            throw new NotImplementedException();
+            return elements.ContainsKey(element);
         }
 
         public IEnumerable<T> GetCommonElements(Hierarchy<T> other)
@@ -48,5 +68,34 @@
         {
             return GetEnumerator();
         }
+
+        private void ContainsItemOrThrowExeption(T element)
+        {
+            if (!Contains(element))
+            {
+                throw new ArgumentException();
+            }
+        }
+        private Node<T> CreateNode(T element)
+        {
+            var node = new Node<T>(element);
+            elements[element] = node;
+            return node;
+        }
+        private void DestroyElement(T element)
+        {
+            var node = elements[element];
+            node.Parent?.Children.Remove(node);
+            if (node.Parent != null && node.Children.Count > 0)
+            {
+                foreach (var child in node.Children)
+                {
+                    child.Parent = node.Parent;
+                    node.Parent.Children.Add(child);
+                }
+            }
+            elements.Remove(element);
+        }
+
     }
 }
