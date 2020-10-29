041242669c52d293236c4465e75fa81d3129cc3b
Kristina <kriska2000@abv.bg>
2020-10-30 00:53:09 +0200

    complete first task

--- Advanced/01. B-Trees-2-3-Trees-and-AVL-Trees/Exercise/02. B-Trees-2-3-Trees-and-AVL-Trees-Exercise-Skeleton/02.Two-Three/TwoThreeTree.cs
+++ Advanced/01. B-Trees-2-3-Trees-and-AVL-Trees/Exercise/02. B-Trees-2-3-Trees-and-AVL-Trees-Exercise-Skeleton/02.Two-Three/TwoThreeTree.cs
@@ -9,9 +9,8 @@
 
         public void Insert(T key)
         {
-            // TODO
+            root = Insert(root, key);
         }
-
 
         public override String ToString()
         {
@@ -51,6 +50,101 @@
                 RecursivePrint(node.LeftChild, sb);
                 RecursivePrint(node.MiddleChild, sb);
                 RecursivePrint(node.RightChild, sb);
+            }
+        }
+
+        private TreeNode<T> Insert(TreeNode<T> node, T key)
+        {
+            if (node == null)
+            {
+                return new TreeNode<T>(key);
+            }
+
+            TreeNode<T> returnNode;
+
+            if (node.IsLeaf())
+            {
+                return null;
+            }
+
+            if (key.CompareTo(node.LeftKey) < 0)
+            {
+                returnNode = Insert(node.LeftChild, key);
+                if (returnNode == node.LeftChild)
+                {
+                    return node;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else if (node.IsTwoNode() || key.CompareTo(node.RightKey) < 0)
+            {
+                returnNode = Insert(node.MiddleChild, key);
+                if (returnNode == node.MiddleChild)
+                {
+                    return node;
+                }
+                else
+                {
+                    return null;  //todo
+                }
+            }
+            else
+            {
+                returnNode = Insert(node.RightChild, key);
+                if (returnNode == node.RightChild)
+                {
+                    return node;
+                }
+                else
+                {
+                    return null; //todo
+                }
+            }
+        }
+
+        private TreeNode<T> InsertMiddleNode(TreeNode<T> a, TreeNode<T> b)
+        {
+            if (a.RightKey == null)
+            {
+                if (a.LeftKey.CompareTo(b.LeftKey)<0)
+                {
+                    a.RightKey = b.LeftKey;
+                    a.MiddleChild = b.LeftChild;
+                    a.RightChild = b.MiddleChild;
+                }
+                else
+                {
+                    a.RightKey = a.LeftKey;
+                    a.RightChild = a.MiddleChild;
+                    a.LeftKey = b.LeftKey;
+                    a.MiddleChild = b.MiddleChild;
+                }
+            }
+            else if (a.LeftKey.CompareTo(b.LeftKey) >= 0)
+            {
+                TreeNode<T> mergeNode = new TreeNode<T>(a.LeftKey)
+                {
+                    LeftChild = b,
+                    MiddleChild = a
+                };
+                b.LeftChild = a.LeftChild;
+                a.LeftChild = a.MiddleChild;
+                a.MiddleChild = a.RightChild;
+                a.RightChild = null;
+                a.LeftKey = a.RightKey;
+                a.RightKey = default(T);
+                return mergeNode;
+            }
+            else if (a.RightKey.CompareTo(b.LeftKey) >= 0)
+            {
+
+            }
+            else
+            {
+
             }
         }
     }
